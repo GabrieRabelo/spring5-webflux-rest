30df54014adc1a44e7a4045eb9b796817515f7af
gabrierabelo <gabriel.almeida97@gmail.com>
2020-02-17 14:18:54 -0300

    first commit

---
+++ src/test/java/guru/springframework/spring5webfluxrest/Spring5WebfluxRestApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5webfluxrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebfluxRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
