d1dc65d50eb8febd72a0bcdab4d203fdbb71f41e
gabrierabelo <gabriel.almeida97@gmail.com>
2020-02-17 15:07:14 -0300

    adicionada entidade e repositorio de categoria

---
+++ src/main/java/guru/springframework/spring5webfluxrest/domain/Category.java
@@ -0,0 +1,15 @@
+package guru.springframework.spring5webfluxrest.domain;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+public class Category {
+
+    @Id
+    private String id;
+
+    private String description;
+}
