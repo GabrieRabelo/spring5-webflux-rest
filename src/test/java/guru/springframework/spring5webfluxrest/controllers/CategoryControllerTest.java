a24baccccbfc40ecb4080aa08938f29f963ae3d5
gabrierabelo <gabriel.almeida97@gmail.com>
2020-02-18 13:32:57 -0300

    h2/inicia criação de controllers para categoria, e testes

---
+++ src/test/java/guru/springframework/spring5webfluxrest/controllers/CategoryControllerTest.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring5webfluxrest.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void list() {
+    }
+
+    @Test
+    void getById() {
+    }
+}
