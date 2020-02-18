d8a766a1771a631b736189b3baa3982a3c267493
gabrierabelo <gabriel.almeida97@gmail.com>
2020-02-18 15:18:47 -0300

    h2/cria teste para controller de category

--- src/test/java/guru/springframework/spring5webfluxrest/controllers/CategoryControllerTest.java
+++ src/test/java/guru/springframework/spring5webfluxrest/controllers/CategoryControllerTest.java
@@ -1,21 +1,51 @@
 package guru.springframework.spring5webfluxrest.controllers;
 
+import guru.springframework.spring5webfluxrest.domain.Category;
+import guru.springframework.spring5webfluxrest.repositories.CategoryRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import static org.mockito.ArgumentMatchers.anyString;
 
 class CategoryControllerTest {
 
+    WebTestClient webTestClient;
+    CategoryRepository categoryRepository;
+    CategoryController categoryController;
+
+
     @BeforeEach
     void setUp() {
+        categoryRepository = Mockito.mock(CategoryRepository.class);
+        categoryController = new CategoryController(categoryRepository);
+        webTestClient = WebTestClient.bindToController(categoryController).build();
     }
 
     @Test
     void list() {
+        BDDMockito.given(categoryRepository.findAll())
+                .willReturn(Flux.just(Category.builder().description("cat1").build(),
+                        Category.builder().description("cat2").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/categories")
+                .exchange()
+                .expectBodyList(Category.class)
+                .hasSize(2);
     }
 
     @Test
-    void getById() {
+    void getById() throws Exception {
+        BDDMockito.given(categoryRepository.findById("someid"))
+                .willReturn(Mono.just(Category.builder().description("cat").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/categories/someid")
+                .exchange()
+                .expectBody(Category.class);
     }
 }
