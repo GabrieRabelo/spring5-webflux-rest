757e000a89df6e06d72eb066b703395f8d218848
gabrierabelo <gabriel.almeida97@gmail.com>
2020-02-19 15:25:54 -0300

    h2/cria metodo post para category

--- src/test/java/guru/springframework/spring5webfluxrest/controllers/CategoryControllerTest.java
+++ src/test/java/guru/springframework/spring5webfluxrest/controllers/CategoryControllerTest.java
@@ -6,9 +6,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.any;
 
 class CategoryControllerTest {
 
@@ -46,5 +49,19 @@ class CategoryControllerTest {
                 .uri("/api/v1/categories/someid")
                 .exchange()
                 .expectBody(Category.class);
+    }
+
+    @Test
+    public void testCreateCategory() {
+        BDDMockito.given(categoryRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Category.builder().build()));
+
+        Mono<Category> catToSaveMono = Mono.just(Category.builder().description("aaaa").build());
+
+        webTestClient.post()
+                .uri("/api/v1/categories")
+                .body(catToSaveMono, Category.class)
+                .exchange()
+                .expectStatus().isCreated();
     }
 }
